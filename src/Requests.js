362171c196b4126a92f9ee3318270c63e11ab5b4
himanshi-rathore <108826224+himanshi-rathore@users.noreply.github.com>
2022-10-02 19:17:50 +0530

    before deploy

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key ="b3e3dd1f6f246ecaa4f04f24051c559d";
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+  };
+
+  export default requests ;
