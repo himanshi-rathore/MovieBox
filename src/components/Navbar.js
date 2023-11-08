bf96e7205d4a36454f1b73531925c8ca3affc1fc
Himanshi Rathore <108826224+himanshi-rathore@users.noreply.github.com>
2023-11-09 01:11:38 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,7 +19,7 @@ function Navbar() {
   return (
     <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
     <Link to='/'>
-     <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>Netflix</h1>
+     <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>MovieBox</h1>
      </Link>
      {user?.email ? (
   <div>
